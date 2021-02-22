ba82ec2474525ccc778625d9bf4ff65454e772d8
Benjamin Papin <ben.papin@paconsulting.com>
2021-02-22 14:49:49 +0000

    Jump to mark after calling quit.

--- lua/hop/init.lua
+++ lua/hop/init.lua
@@ -155,7 +155,8 @@ end
 function M.quit(buf_handle)
   if vim.b['hop#marked'] then
     vim.api.nvim_buf_delete(buf_handle, { force = true })
-    vim.cmd('echohl Error|echo "no buf_handle = ' .. buf_handle .. '"')
+    --vim.cmd('echohl Error|echo "no buf_handle = ' .. buf_handle .. '"')
+    vim.cmd("normal `'")
   end
 end
 
